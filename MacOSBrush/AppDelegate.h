f8ea407ea437bf5ea55e019eebcbf7ee2e5c78be
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2015-06-17 14:35:08 +0600

    Initial Commit

---
+++ MacOSBrush/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacOSBrush
+//
+//  Created by neo on 17.06.15.
+//  Copyright (c) 2015 NSSimpleApps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MacOSBrush/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MacOSBrush
+//
+//  Created by neo on 17.06.15.
+//  Copyright (c) 2015 NSSimpleApps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
